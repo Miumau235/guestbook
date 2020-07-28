39a3b7cbdd10b9dc4291392b4e9f2121665ff2f5
Lilla  Gerstenbrand <lilla.gerstenbrand@siili.com>
2020-07-28 14:29:56 +0300

    reagent, cljs and whatnot

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -2,21 +2,39 @@
   (:require
    [guestbook.layout :as layout]
    [guestbook.db.core :as db]
-   [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [struct.core :as st]
+   [ring.util.http-response :as response]
+   [guestbook.validation :refer [validate-message]]))
 
 (defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+  (layout/render
+   request
+   "home.html"))
 
 (defn about-page [request]
   (layout/render request "about.html"))
+
+(defn message-list [_]
+  (response/ok {:messages (vec (db/get-messages))}))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (response/bad-request {:errors errors})
+    (try
+      (db/save-message! params)
+      (response/ok {:status :ok})
+      (catch Exception e
+        (response/internal-server-error
+         {:errors {:server-error ["Failed to save message!"]}})))))
 
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/messages" {:get message-list}]
+   ["/message" {:post save-message!}]
    ["/about" {:get about-page}]])
 
--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -1,13 +1,11 @@
 (ns guestbook.test.db.core
   (:require
    [guestbook.db.core :refer [*db*] :as db]
-   [java-time.pre-java8]
    [luminus-migrations.core :as migrations]
    [clojure.test :refer :all]
    [clojure.java.jdbc :as jdbc]
    [guestbook.config :refer [env]]
    [mount.core :as mount]))
-
 (use-fixtures
   :once
   (fn [f]
@@ -16,23 +14,16 @@
      #'guestbook.db.core/*db*)
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
-
-(deftest test-users
+(deftest test-messages
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
+    (is (= 1 (db/save-message!
               t-conn
-              {:id         "1"
-               :first_name "Sam"
-               :last_name  "Smith"
-               :email      "sam.smith@example.com"
-               :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+              {:name "Bob"
+               :message "Hello, World"}
+              {:connection t-conn})))
+    (is (= {:name "Bob"
+            :message "Hello, World"}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message]))))))
